3d36718412e8a8946f67836fc33c83d2c401510b
Shivendra <kalseshivendra@gmail.com>
2021-09-25 00:58:16 +0530

    Unit testing

--- src/main/java/com/nighteye/springbootcrud/controller/UserController.java
+++ src/main/java/com/nighteye/springbootcrud/controller/UserController.java
@@ -8,13 +8,7 @@ import com.nighteye.springbootcrud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -90,5 +84,10 @@ public class UserController {
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @RequestMapping("/home")
+    public String homePage() {
+        return "Welcome to user DB.";
     }
 }
---
+++ src/test/java/com/nighteye/springbootcrud/HttpRequestTest.java
@@ -0,0 +1,31 @@
+package com.nighteye.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/home",
+                String.class)).contains("Welcome to user DB.");
+    }
+
+    @Test
+    public void yourTestNames() throws Exception{
+        //Write your tests here
+
+    }
+}
